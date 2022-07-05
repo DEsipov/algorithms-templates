ec9fca1a1a6706ecdf30b383b8add70f7480d9d5
JustWork Robot <robot@just-work.org>
2022-07-05 18:03:10 +0300

    Застежка-молния.

--- python/sprint0/b.py
+++ python/sprint0/b.py
@@ -1,14 +1,31 @@
 from typing import List, Tuple
 
+"""
+Даны два массива чисел длины n. Составьте из них один массив длины 2n, 
+в котором числа из входных массивов чередуются 
+(первый — второй — первый — второй — ...). 
+При этом относительный порядок следования чисел из одного массива должен 
+быть сохранён.
+"""
+
+
 def zipper(a: List[int], b: List[int]) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    """"""
+    n = len(a)
+    res = []
+    for i in range(0, n):
+        res.append(a[i])
+        res.append(b[i])
+
+    return res
+
 
 def read_input() -> Tuple[List[int], List[int]]:
     n = int(input())
     a = list(map(int, input().strip().split()))
     b = list(map(int, input().strip().split()))
     return a, b
+
 
 a, b = read_input()
 print(" ".join(map(str, zipper(a, b))))
