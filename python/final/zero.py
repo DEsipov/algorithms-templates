d740ae4611ebd138512980800896be3ef8d16c0c
JustWork Robot <robot@just-work.org>
2022-07-11 21:41:19 +0300

    Zero финал, версия рабочая.

--- python/final/zero.py
+++ python/final/zero.py
@@ -1,79 +1,38 @@
 #!-*-coding:utf-8-*-
-
-# Тимофей ищет место, чтобы построить себе дом. Улица, на которой он хочет
-# жить, имеет длину n, то есть состоит из n одинаковых идущих подряд участков.
-
-# Каждый участок либо пустой, либо на нём уже построен дом.
-# Общительный Тимофей не хочет жить далеко от других людей на этой улице.
-
-# Поэтому ему важно для каждого участка знать расстояние до ближайшего
-# пустого участка.
-
-# Если участок пустой, эта величина будет равна нулю —
-# расстояние до самого себя.
-
-# Помогите Тимофею посчитать искомые расстояния. Для этого у вас есть
-# карта улицы. Дома в городе Тимофея нумеровались в том порядке, в котором
-# строились, поэтому их номера на карте никак не упорядочены.
-# Пустые участки обозначены нулями.
-
-
-# Формат ввода
-# В первой строке дана длина улицы —– n (1 ≤ n ≤ 106). В следующей
-# строке записаны n целых неотрицательных чисел — номера домов и
-# обозначения пустых участков на карте (нули). Гарантируется,
-# что в последовательности есть хотя бы один ноль. Номера домов
-# (положительные числа) уникальны и не превосходят 109.
-
-# Формат вывода
-# Для каждого из участков выведите расстоя\ние до ближайшего нуля.
-# Числа выводите в одну строку, разделяя их пробелами.
-
-# Пример данных.
-# n = 5
-# lst = [0, 1, 4, 9, 0]
-# res = [0, 1, 2, 1, 0]
-import unittest
-from typing import List, Tuple
 from unittest import TestCase
 
 
+def get_forward_lst(lst):
+
+    res = []
+    d = len(lst)
+    for n in lst:
+        if n == 0:
+            d = 0
+        else:
+            d += 1
+
+        res.append(d)
+
+    return res
+
+
 def zeronear(lst):
-    n = len(lst)
-    forward = [0] * n
-    backward = [0] * n
-    for i in range(0, n):
-        # forward
-        j = i
-        while lst[j] != 0:
-            forward[i] += 1
-            j += 1
-            if j == n:
-                forward[i] = n
-                break
-
-        # backward
-        j = n - 1 - i
-        while lst[j] != 0:
-            backward[i] += 1
-            j -= 1
-            if j < 0:
-                backward[i] = n
-                break
-
-    backward.reverse()
-    return list(map(min, zip(forward, backward)))
+    to_left = get_forward_lst(lst)
+    to_right = tuple(reversed(get_forward_lst(list(reversed(lst)))))
+    return list(map(min, zip(to_left, to_right)))
 
 
-def read_input() -> Tuple[List[int], List[int]]:
-    n = int(input())
-    return list(map(int, input().strip().split()))
-
-
-lst = read_input()
-
-result = zeronear(lst)
-print(" ".join(map(str, result)))
+################### Для Ya ###################
+# def read_input() -> Tuple[List[int], List[int]]:
+#     n = int(input())
+#     return list(map(int, input().strip().split()))
+#
+#
+# lst = read_input()
+# result = zeronear(lst)
+# print(" ".join(map(str, result)))
+################### Для Ya ###################
 
 
 class ZeroTestCase(TestCase):
@@ -96,10 +55,7 @@ class ZeroTestCase(TestCase):
         self.assertEqual(expected_data, res)
 
 
-# if __name__ == '__main__':
-#     ts = ZeroTestCase()
-#     ts.test_smoke_1()
-#     ts.test_smoke_2()
-
-
-
+if __name__ == '__main__':
+    ts = ZeroTestCase()
+    ts.test_smoke_1()
+    ts.test_smoke_2()
