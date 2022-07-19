eabf668886f47bdd172b86e30c3e21da38a36f98
JustWork Robot <robot@just-work.org>
2022-07-19 22:32:11 +0300

    Нелюбимое дело, решение.

--- python/sprint12/unlike_todo.py
+++ python/sprint12/unlike_todo.py
@@ -11,7 +11,7 @@
 input
 4
 abra
-ca dabra
+cdabra
 boom
 oops
 0
@@ -31,22 +31,51 @@ class Node:
         self.next_item = next_item
 
 
-def solution(node, index_for_delete):
-    # Your code
-    ...
+def print_list(node):
+    while node is not None:
+        print(node.value)
+        node = node.next_item
 
 
-if __name__ == '__main__':
+def create_list():
     count = int(input())
 
-    # Создание однонаправленного списка.
     node = None
     head = None
+
     for _ in range(count):
         value = input()
         node = Node(value, next_item=node)
         if head is None:
             head = node
 
-    # Вывод списка.
-    solution(node)
+    return head
+
+
+def get_node_by_index(node, index):
+    """Получить элемент по индексу."""
+    while index:
+        node = node.next_item
+        index -= 1
+    return node
+
+
+def remove_by_index(head, idx_delete):
+    if idx_delete == 0:
+        return get_node_by_index(head, idx_delete + 1)
+
+    prev = get_node_by_index(head, idx_delete - 1)
+    prev.next_item = prev.next_item and prev.next_item.next_item
+
+    return head
+
+
+if __name__ == '__main__':
+    head = create_list()
+
+    inx_delete = int(input())
+
+    # Удаление.
+    head = remove_by_index(head, inx_delete)
+    # Вывод списка.Вывод списка.
+    print_list(head)
