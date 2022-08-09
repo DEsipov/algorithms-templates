f495452d566024a2435184102171e2d51fedaaea
JustWork Robot <robot@just-work.org>
2022-08-09 10:25:40 +0300

    Спринт 13. Разность треш-индексов.

--- python/sprint13/O_trash_index_3.py
+++ python/sprint13/O_trash_index_3.py
@@ -1,25 +1,42 @@
 #!-*-coding:utf-8-*-
-"""Реализация с односвязным списком."""
+"""Реализация с chunk."""
+
+CHUNK = 50
+
+
+def reload(lst, k):
+    lst_ord = sorted(lst)[:k]
+    del lst
+    return sorted(lst_ord)[:k]
 
 
 def calc(lst, k):
+
     res = []
+    max_item = None
 
     for i in range(len(lst) - 1):
         for j in range(i + 1, len(lst)):
             item = abs(lst[i] - lst[j])
-            res.append(item)
 
-    res = sorted(res)
-    # print(res)
+            if max_item is None:
+                res.append(item)
+            elif item < max_item:
+                res.append(item)
+
+            if len(res) == CHUNK:
+                res = reload(res, k)
+                max_item = res[-1]
+
+    res = reload(res, k)
     return res[k - 1]
 
 
 if __name__ == '__main__':
     # Пример 1
-    n = 3
-    line = '1 3 5'
-    k = 3
+    # n = 3
+    # line = '1 3 5'
+    # k = 3
     # expected = 4
 
     # Пример 2
@@ -29,7 +46,7 @@ if __name__ == '__main__':
     # expected = 0
 
     # Пример 3
-    # n =3
+    # n = 3
     # line = '9 6 10 3'
     # k = 3
     # expected = 3
@@ -39,7 +56,7 @@ if __name__ == '__main__':
     line = input()
     k = int(input())
 
-    lst = list(map(int, line.split()))
+    lst = tuple(map(int, line.split()))
 
     res = calc(lst, k)
     print(res)
